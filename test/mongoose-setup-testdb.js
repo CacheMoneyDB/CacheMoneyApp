e72a7f04e3b3a216f8a35d5d0db13c3ab9066d79
nsmeds <niilosmeds@gmail.com>
2016-11-14 16:59:11 -0800

    Added first e2e test

---
+++ test/mongoose-setup-testdb.js
@@ -0,0 +1,35 @@
+const mongoose = require( 'mongoose' );
+
+// we need a URI that points to our database
+const dbURI = process.env.MONGODB_URI || 
+'mongodb://niilos:223codefellows@ds153667.mlab.com:53667/endtoendtestdb'
+ || 'mongodb://localhost/cachemoneydev'; 
+
+mongoose.Promise = Promise;
+mongoose.connect( dbURI ); 
+
+// CONNECTION EVENTS
+// When successfully connected
+mongoose.connection.on('connected', function () {  
+    console.log( 'Mongoose default connection open to ' + dbURI );
+}); 
+
+// If the connection throws an error
+mongoose.connection.on('error',function (err) {  
+    console.log( 'Mongoose default connection error: ' + err );
+}); 
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', function () {  
+    console.log( 'Mongoose default connection disconnected' ); 
+});
+
+// If the Node process ends, close the Mongoose connection 
+process.on('SIGINT', function() {  
+    mongoose.connection.close(function () { 
+        console.log( 'Mongoose default connection disconnected through app termination' ); 
+        process.exit(0); 
+    }); 
+});
+
+module.exports = mongoose.connection;
