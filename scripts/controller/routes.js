6d89da863af263836f94826ea1cc79769c7144d9
nsmeds <niilosmeds@gmail.com>
2016-11-15 14:04:07 -0800

    went back to five routes (separate search and portfolio

--- scripts/controller/routes.js
+++ scripts/controller/routes.js
@@ -2,6 +2,7 @@
 
 page('/', loginCtrl.show);
 page('/login', loginCtrl.show);
+page('/search', searchCtrl.show);
 page('/portfolio', portfolioCtrl.show);
 page('/leaderboard', leaderboardCtrl.show);
 page('/about', aboutCtrl.show);
