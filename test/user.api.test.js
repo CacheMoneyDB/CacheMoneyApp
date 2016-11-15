9a4c60b080233bde6f89542865652290473c7050
Michelle Sri <michelle.sri@gmail.com>
2016-11-15 11:16:48 -0800

    finished user auth tests. made small fixes to error handler need to add admin access tests.

--- test/user.api.test.js
+++ test/user.api.test.js
@@ -6,47 +6,103 @@ const connection = require('./mongoose-setup-testdb');
 
 chai.use(chaiHttp);
 
-describe('end to end tests for user', () => {
 
-    const request = chai.request(app);
+const user = {
+    username: 'awesomeUser',
+    password: 'awesomePassword'
+};
+
+const userAdmin = {
+    username: 'userAdmin',
+    password: 'adminPassword'
+};
+
+//set another user named admin
+//make sure admin can use things like delete
+
+//portfolios api tests.
+
+//test yahoo api.
+
+describe('user management', () => {
 
     before(done => {
         const drop = () => connection.db.dropDatabase(done);
-        if (connection.readyState === 1) drop();
+        if(connection.readyState === 1) drop();
         else connection.on('open', drop);
     });
+    const request = chai.request(app);
 
-    const Michael = {
-        username: 'Michael Burry',
-        password: 'thebigshort'
-    };
+    function badRequest(url, send, error, done){
+        request
+          .post(url)
+          .send(send)
+          .then(() => done('status should not be 200'))
+          .catch(res => {
+              assert.equal(res.status, 400);
+              assert.equal(res.response.body.error, error);
+              done();
+          })
+          .catch(done);
+    }
+
+    it('signup requires username', done => {
+        badRequest('/users/signup', {password:'coolPassword'}, 'Valid username and password required', done);
+    });
+
+    it('signup requires password', done => {
+        badRequest('/users/signup', {username:'coolUsername'}, 'Valid username and password required', done);
+    });
 
     let token = '';
 
-    it('signs up a new user', done => {
+    it('/users/signup', done => {
         request
-          .post('/users/signup')
-          .send(Michael)
-          .then(res => {
-              assert.isOk(res.body.token);
-              token = res.body.token;
-              done();
-          })
-          .catch(err => done(err));
+      .post('/users/signup')
+      .send(user)
+      .then(res => assert.isOk( token = res.body.token ))
+      .then( done, done );
     });
 
-    it('signs in an existing user', done => {
-        request
-          .post('/users/signin')
-          .send(Michael)
-          .then(res => {
-              assert.isOk(res.body.token);
-              token = res.body.token;
-              done();
-          })
-        .catch(err => done(err));
+    it('cannot be the same username', done => {
+        badRequest('/users/signup', user, 'username awesomeUser already exists', done);
     });
 
+    it('token is valid', done => {
+        request
+      .get('/users/signin')
+      .set('authorization', `Bearer ${token}`)
+      .then(res => assert.isOk(res.body))
+      .then(done, done);
+    });
 
+// signin and get a token back
+// make a new request with the token to the validate api path
+// expect this request to return true
 
+    it('signin', function(done){
+        request
+      .post('/users/signin')
+      .send(user)
+      .then(res => {
+          request
+          .post('/users/validate')
+          .set('authorization', `Bearer ${res.body.token}`)
+          .then(res => {
+              assert.isOk(res.body.valid);
+              done();
+          });
+      })
+      .catch(done);
+        // assert.equal(res.body.token, token}))
+      // .then(done, done);
+      // .then((err, res) => {
+      // if(err)done(err);
+      // done();
+    // })
+    });
+
+    describe('user admin access', () => {
+
+    });
 });
