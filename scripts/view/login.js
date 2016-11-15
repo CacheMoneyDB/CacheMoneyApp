663ed53c32a40a38c18a6a83936f4c4537b4b758
nsmeds <niilosmeds@gmail.com>
2016-11-15 15:08:59 -0800

    Working on login.js

--- scripts/view/login.js
+++ scripts/view/login.js
@@ -1,12 +1,26 @@
 (function(module) {
-    $('#signup-button').on('submit', function(event) {
-        event.preventDefault();
-        $.ajax({
-            url: 'users/signup',
-            method: 'POST',
-            data: ,
-            success: 
-        })
+    // const users = require('../lib/routes/users');
+    var login = {};
 
-    })
+    login.newUser = function() {
+        $('#signup-button').on('click', function(event) {
+            event.preventDefault();
+            let username = $('#new-user-name').val();
+            // let email = $('#new-user-email').val();
+            let password = $('#new-user-pw').val();
+            $.ajax({
+                url: '/users/signup',
+                type: 'POST',
+                data: {username: username, password: password}
+            })
+            .done(function(user) {
+                console.log('new user created: ', user);
+            });
+        });
+    };
+
+    login.newUser();
+    
+    module.login = login;
+
 })(window);
