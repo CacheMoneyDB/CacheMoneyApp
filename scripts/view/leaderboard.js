aea70275c5ed8b9705e1d24c12f3c53c3f03a904
Christopher Bruner <chrbruner9@gmail.com>
2016-11-16 14:32:56 -0800

    Another hotfix implemented

--- scripts/view/leaderboard.js
+++ scripts/view/leaderboard.js
@@ -13,11 +13,6 @@
                 contentType: 'application/json',
                 url: '/portfolios/leaderboard'
             }).done(function(data){
-<<<<<<< HEAD
-                console.log('leaderboardData', data);
-                leaderBoardData = data;
-                leaderBoardData.renderStock();
-=======
                 // console.log('Data', data);
                 $('#leaderboard').empty();
                 module.leaderBoardModel.formatData(data, function(err, formattedData){
@@ -25,7 +20,6 @@
                         leaderBoardData.renderStock(leaderboardEntry);
                     });
                 });
->>>>>>> 336e1ba7a802a48d91db6f3bd46f1be6ce5c9533
             }).fail(function(jqxhr, status){
                 console.log('leaderboard AJAX request has failed', status, jqxhr);
             });
