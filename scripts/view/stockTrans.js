1edb96263dbe2105d0b49522088d5f8af13a3f13
TomTimmel <tom.t.timmel@gmail.com>
2016-11-15 16:56:59 -0800

    ajax cashValue call and finish stocktrans ajax call

--- scripts/view/stockTrans.js
+++ scripts/view/stockTrans.js
@@ -7,37 +7,34 @@
     stockTrans.addButton = function() {
         $('#buy-button').on('click', function (event){
             event.preventDefault();
-            stockTrans.shares = $(this).prev().val();
+            stockTrans.shares = parseInt($(this).prev().val());
             stockTrans.stock = stockSearch.data.symbol;
             stockTrans.price = stockSearch.data.ask;
-            stockTrans.cashEffect = -(stockTrans.shares * stockTrans.price);
+            // stockTrans.cashEffect = -(stockTrans.shares * stockTrans.price);
             var dataToSend = {stock: stockTrans.stock, shares: stockTrans.shares, price: stockTrans.price};
             dataToSend = JSON.stringify(dataToSend);
             console.log('datatosend', dataToSend);
             $.ajax({
+                headers:{
+                    Authorization: 'Bearer ' + module.localStorage.getItem('token')
+                },
+                contentType: 'application/json',
                 url: '/portfolios/buy',
                 type: 'PUT',
                 data: dataToSend                
             }).done(function(data){
-                console.log('Data', dataToSend);
+                console.log('data', data.cashValue);
+                // renderCashValue(data);
             }).fail(function(jqxhr, status){
                 console.log('buy AJAX request has failed', status, jqxhr);
             });
-        });
-        $.ajax({
-            url:'/portfolios' + stockTrans.stock
-        }).done(function(data){
-            console.log('Data', data);
-            renderCashValue(data);
-        }).fail(function(jqxhr, status){
-            console.log('cash value AJAX request has failed', status, jqxhr);
         });
 
     };
 
     stockTrans.addButton();
 
-    data.renderCashValue = function(){
+    renderCashValue = function(data){
         $('#account-info').empty().append(cashValueCompiler(data.cashValue));
     };
 
