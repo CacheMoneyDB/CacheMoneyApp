46d435b2806b402ea580832a07a62f0f209c4dcf
TomTimmel <tom.t.timmel@gmail.com>
2016-11-17 15:06:53 -0800

    create table for leaderboard and css

--- scripts/view/leaderboard.js
+++ scripts/view/leaderboard.js
@@ -14,10 +14,9 @@
                 url: '/portfolios/leaderboard'
             }).done(function(data){
 
-                // console.log('Data', data);
-                $('#leaderboard').empty();
+                $('#leaderboard-table').empty();
                 module.leaderBoardModel.formatData(data, function(err, formattedData){
-                    $('#leaderboard').append('<h1>Leaderboard</h1>');
+                    $('#leaderboard-table').append('<tr><th>Rank</th><th>Name</th><th>Cash Value</th><th>Stock Value</th><th>Total Value</th></tr>');
                     formattedData.forEach(function (leaderboardEntry){
                         leaderBoardData.renderStock(leaderboardEntry);
                     });
@@ -35,7 +34,7 @@
         leaderBoardDataEntry.cashValue = (Math.round(leaderBoardDataEntry.cashValue*Math.pow(10,2))/Math.pow(10,2)).toFixed(2);
         leaderBoardDataEntry.netValue = (Math.round(leaderBoardDataEntry.netValue*Math.pow(10,2))/Math.pow(10,2)).toFixed(2);
         leaderBoardDataEntry.stockValue = (Math.round(leaderBoardDataEntry.stockValue*Math.pow(10,2))/Math.pow(10,2)).toFixed(2);
-        $('#leaderboard').append(leaderCompiler(leaderBoardDataEntry));
+        $('#leaderboard-table').append(leaderCompiler(leaderBoardDataEntry));
     };
 
     module.leaderBoardData = leaderBoardData;
